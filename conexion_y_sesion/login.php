d47eef1dd2abb207b8251a725e6ba7610f207f3d
CeciiBloise <bloisececii@gmail.com>
2023-04-05 09:48:55 -0300

    Cambios en el login

--- conexion_y_sesion/login.php
+++ conexion_y_sesion/login.php
@@ -1,6 +1,5 @@
 <?php
 /*** VALIDACION DE USUARIO ***/
-/**ESTO ANDAAAAAAAAA */
 session_start();
 include("conexion.php");
 $conexion=conectar();
@@ -53,27 +52,30 @@ if(mysqli_num_rows($query) == 1)
         exit();
       }
     } else {
-      // Mostrar un mensaje de error si el usuario o la contraseña son incorrectos
-            echo "<body class='p-3 m-0 border-0 bd-example'>";
-            echo "<div class='alert alert-danger d-flex align-items-center' role='alert'>";
-            echo "<svg class='bi flex-shrink-0 me-2' role='img' aria-label='Peligro:'>";
-            echo "<use xlink:href='#exclamation-triangle-fill'></use>";
-            echo "</svg>";
-            echo "<div><font style='vertical-align: inherit;'><font style='vertical-align: inherit;'>";
-            echo "La contraseña o usiario son incorrectas. Haz <a href='../index.html' class='alert-link'>click aquì</a> para volver a intentarlo.";
-            echo "</font></font></div>";
-            echo "</div>";
-            echo "</body>";
+    ?>
+    
+    <!DOCTYPE html>
+    <html>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+        <title>Trenes Argentinos</title>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>       
+        </head>
+    </html>
+    <!-- Mostrar un mensaje de error si el usuario o la contraseña son incorrectos -->
+        <body class='p-3 m-0 border-0 bd-example'>
+            <div class='alert alert-danger d-flex align-items-center' role='alert'>
+                <svg class='bi flex-shrink-0 me-2' role='img' aria-label='Peligro:'>
+                    <use xlink:href='#exclamation-triangle-fill'></use>
+                </svg>
+                <div><font style='vertical-align: inherit;'><font style='vertical-align: inherit;'>
+                    <p>La contraseña o usiario son incorrectas. Haz <a href='../index.html' class='alert-link'>click aquí</a> para volver a intentarlo.</p>
+                 </font></font></div>
+            </div>
+        </body>
+    <?php
     }
     ?>
-<!DOCTYPE html>
-<html>
-    <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Trenes Argentinos</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>       
-    </head>
-</html>
